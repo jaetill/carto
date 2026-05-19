d3a28d6a42890838a632143d81a3c9ca8b86287b
Jason Tilley <tilley_jason@hotmail.com>
2026-05-19 06:00:16 -0400

    feat: Phase 7 - user feedback widget + Lambda
    Standard 11. New POST /feedback on carto REST API (auth=NONE). ESM Lambda (matches carto handler style). Needs PAT in secret carto/github-token.

--- terraform/envs/prod/imports.tf
+++ terraform/envs/prod/imports.tf
@@ -1,9 +1,1 @@
-import {
-  to = aws_secretsmanager_secret.main
-  id = "arn:aws:secretsmanager:us-east-2:214599503944:secret:carto/secrets-qgjImO"
-}
-
-import {
-  to = aws_cloudwatch_log_group.cartoapi
-  id = "/aws/lambda/cartoApi"
-}
+# Phase 6 + Phase 7 applied 2026-05-19.
