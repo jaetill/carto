022b69781d1db8db6e6e7e3f38060212b6a7c853
Jason Tilley <tilley_jason@hotmail.com>
2026-05-18 23:38:27 -0400

    feat(iac): Slice 4 (API Gateway) - CartoAPI REST + perms
    REST API (9o7c3668a4): 1 api (REGIONAL?) + 5 resources (engagements, engagement, engagement/{id}, engagement/{id}/data, engagement/{id}/snapshots) + methods + integrations + deployment + stage + 1 lambda_permission.

--- terraform/envs/prod/imports.tf
+++ terraform/envs/prod/imports.tf
@@ -1,1 +1,160 @@
-# Slices 1-3 applied 2026-05-19.
+import {
+  to = aws_api_gateway_rest_api.carto_rest
+  id = "9o7c3668a4"
+}
+
+import {
+  to = aws_api_gateway_authorizer.carto_rest_cartosharedpool
+  id = "9o7c3668a4/b7mlmb"
+}
+
+import {
+  to = aws_api_gateway_resource.carto_rest_engagement_id_snapshots
+  id = "9o7c3668a4/37ysc6"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_snapshots_get
+  id = "9o7c3668a4/37ysc6/GET"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_snapshots_get
+  id = "9o7c3668a4/37ysc6/GET"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_snapshots_options
+  id = "9o7c3668a4/37ysc6/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_snapshots_options
+  id = "9o7c3668a4/37ysc6/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_method_response.carto_rest_engagement_id_snapshots_options_200
+  id = "9o7c3668a4/37ysc6/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_integration_response.carto_rest_engagement_id_snapshots_options_200
+  id = "9o7c3668a4/37ysc6/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_snapshots_post
+  id = "9o7c3668a4/37ysc6/POST"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_snapshots_post
+  id = "9o7c3668a4/37ysc6/POST"
+}
+
+import {
+  to = aws_api_gateway_resource.carto_rest_engagement_id_data
+  id = "9o7c3668a4/57l8fk"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_data_get
+  id = "9o7c3668a4/57l8fk/GET"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_data_get
+  id = "9o7c3668a4/57l8fk/GET"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_data_options
+  id = "9o7c3668a4/57l8fk/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_data_options
+  id = "9o7c3668a4/57l8fk/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_method_response.carto_rest_engagement_id_data_options_200
+  id = "9o7c3668a4/57l8fk/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_integration_response.carto_rest_engagement_id_data_options_200
+  id = "9o7c3668a4/57l8fk/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagement_id_data_post
+  id = "9o7c3668a4/57l8fk/POST"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagement_id_data_post
+  id = "9o7c3668a4/57l8fk/POST"
+}
+
+import {
+  to = aws_api_gateway_resource.carto_rest_engagements
+  id = "9o7c3668a4/p7c2q0"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagements_get
+  id = "9o7c3668a4/p7c2q0/GET"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagements_get
+  id = "9o7c3668a4/p7c2q0/GET"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagements_options
+  id = "9o7c3668a4/p7c2q0/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagements_options
+  id = "9o7c3668a4/p7c2q0/OPTIONS"
+}
+
+import {
+  to = aws_api_gateway_method_response.carto_rest_engagements_options_200
+  id = "9o7c3668a4/p7c2q0/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_integration_response.carto_rest_engagements_options_200
+  id = "9o7c3668a4/p7c2q0/OPTIONS/200"
+}
+
+import {
+  to = aws_api_gateway_method.carto_rest_engagements_post
+  id = "9o7c3668a4/p7c2q0/POST"
+}
+
+import {
+  to = aws_api_gateway_integration.carto_rest_engagements_post
+  id = "9o7c3668a4/p7c2q0/POST"
+}
+
+import {
+  to = aws_api_gateway_deployment.carto_rest_prod
+  id = "9o7c3668a4/qxfnjh"
+}
+
+import {
+  to = aws_api_gateway_stage.carto_rest_prod
+  id = "9o7c3668a4/prod"
+}
+
+
+import {
+  to = aws_lambda_permission.apigw_cartoapi
+  id = "cartoApi/apigateway-invoke"
+}
